5002f35d8bc5013488f7d3fb4184139cf3502ae7
Nesho RADEV <40929562+RedPRO16@users.noreply.github.com>
2022-08-24 16:40:41 +0100

    challenge -step2

--- Phrase_Two/Step-2/searchCandies.test.js
+++ Phrase_Two/Step-2/searchCandies.test.js
@@ -4,8 +4,17 @@ const searchCandies = require('./searchCandies');
 
 describe('searchcandies', (search, price) => {
 
-  it(' searches "Ma" upto price 10', () =>
-  expect(searchCandies('Ma', 10)).toEqual (['Mars', 'Maltesers']))
+  it('searches "Ma" upto price 10', () =>
+  expect(searchCandies('Ma', 10)).toEqual(['Mars', 'Maltesers']))
+
+  it('searches "Ma" upto price 2', () =>
+  expect(searchCandies('Ma', 2)).toEqual(['Mars']))
+
+  it('searches "S" upto price 10', () =>
+  expect(searchCandies('S', 10)).toEqual([ 'Skitties', 'Skittles', 'Starburst' ]))
+
+  it('searches "S" upto price 4', () =>
+  expect(searchCandies('S', 4)).toEqual([ 'Skitties', 'Skittles']))
 
 })
 
