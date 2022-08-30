7da2cc505333fda72f03ad821a2a5616561936e8
Nesho RADEV <40929562+RedPRO16@users.noreply.github.com>
2022-08-30 12:16:13 +0100

    done

--- Phrase_Two/Step-7/server.js
+++ Phrase_Two/Step-7/server.js
@@ -2,8 +2,24 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+let counter = 0
+
 app.get('/', (req, res) => {
-  res.send('Hello, Neshooooo!')
+  res.send('One is the first number after 0')
+});
+
+app.post('/counter', (req, res) => {
+  counter ++
+  res.send('increase number')
+});
+
+app.get('/counter', (req, res) => {
+  res.send('counter equal ${counter}')
+});
+
+app.delete('/counter', (req, res) => {
+  counter = 0
+  res.send('Reset counter to zero')
 });
 
 console.log(`Server listening on localhost:${port}`);
